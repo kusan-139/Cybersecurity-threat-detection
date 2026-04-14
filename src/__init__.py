c3b9a48e4fcbfec10c15c96d8bf548fd38bf9a4d
Kusan Chakraborty <kusan.c139@gmail.com>
2026-04-14 14:08:51 +0530

    upload

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+# src/__init__.py
+# This file makes 'src' a Python package.
+# It allows you to import modules like: from src.data_loader import load_dataset
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+__project__ = "AI-Powered Cybersecurity Threat Detection System"
